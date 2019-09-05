467924e60f9ec3fedad7407c13fcc5b192715dd7
Joshua Holtsclaw <Jildsmiles@yahoo.com>
2019-09-05 16:51:47 -0400

    forEach bug problems

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,23 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+axios
+  .get("https://api.github.com/users/Marshnme")
+  .then(response => {
+    console.log(response);
+    response.data.login.forEach(item => {
+      const newInfo = createCard(item);
+      entryPoint.appendChild(newInfo);
+    });
+    // const work = response.map(item =>{
+    //   const newInfo = createCard(item);
+    //   entryPoint.appendChild(newInfo);
+    // })
+  })
+  .catch(error => {
+    console.log("The data was not returned", error);
+  });
+  
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -45,6 +61,50 @@ const followersArray = [];
 </div>
 
 */
+const entryPoint = document.querySelector(".cards");
+function createCard(arg){
+  const 
+    newCard = document.createElement("div"),
+    image = document.createElement("img"),
+    cardInfo = document.createElement("div"),
+    name = document.createElement("h3"),
+    userName = document.createElement("p"),
+    location = document.createElement("p"),
+    profile = document.createElement("p"),
+    gitHubLink = document.createElement("a"),
+    followers = document.createElement("p"),
+    following = document.createElement("p"),
+    bio = document.createElement("p");
+
+    newCard.classList.add("card");
+    cardInfo.classList.add("card-info");
+    name.classList.add("name");
+    userName.classList.add("username");
+
+    newCard.appendChild(image);
+    newCard.appendChild(cardInfo);
+    cardInfo.appendChild(name);
+    cardInfo.appendChild(userName);
+    cardInfo.appendChild(location);
+    cardInfo.appendChild(profile);
+    profile.appendChild(gitHubLink);
+    cardInfo.appendChild(followers);
+    cardInfo.appendChild(following);
+    cardInfo.appendChild(bio);
+
+
+    image.src = `${data.avatar_url}`;
+    name.textContent = `${data.name}`;
+    userName.textContent = `${data.login}`;
+    location.textContent = `${data.location}`;
+    profile.textContent = `${data.html_url}`;
+    followers.textContent = `${data.followers}`;
+    following.textContent = `${data.following}`;
+    bio.textContent = `${bio}`;
+
+
+    return newCard;
+};
 
 /* List of LS Instructors Github username's: 
   tetondan
